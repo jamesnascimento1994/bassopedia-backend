e592c736f19cf2e63c8fc901e2db22de8cc3cfba
jamesnascimento1994 <james.nascimento94@yahoo.com>
2020-09-09 16:09:02 -0400

    fixed secret key

--- bass_django/settings.py
+++ bass_django/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5wp4o1z7wfq_+uy)%vgo20s4fg^qk-__qht00fg9&ee@e1@vmz'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ['MODE'] == 'dev' else False
